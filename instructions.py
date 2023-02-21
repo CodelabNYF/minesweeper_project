7ffce35826f5583a79866b599813e91e974930b7
CodelabNYF <nighatfaqah@gmail.com>
2023-02-21 15:40:46 +0500

    Initial Commit

--- instructions.py
+++ instructions.py
@@ -14,8 +14,9 @@ Submission Method:
 3. Add a new file with your own name in the format firstname_lastname.py
 4. The structure of code is present in awais_mazahir.py. Copy the code to newly created file and build your function
 5. Once results are correct, push your code to your github account and initiate a pull request by clicking on "Contribute" Button
+6. Perform the changes in main branch only.
 
-Logic Building:
+Code Requirements:
 
 1. You are required to create logic of minesweeper game. 
 2. Input received by your function will be will be a VARIABLE SIZED List.
